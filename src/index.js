eebb7c18bb27a444d60e65131aae3e201f4ec35c
Satyabrata Saha <108542741+satyabrata-saha@users.noreply.github.com>
2024-10-15 23:19:39 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// require('dotenv').config()
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: "./env"
+})
+
+
+connectDB();
+
+
+
+
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     } catch (err) {
+//         console.error(err);
+//         throw err;
+//     }
+// })()
